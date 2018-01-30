ed475bc2a0c2922915840f25f18d9c24cb5f78e3
Ununhexium116 <ununhaxium@gmail.com>
2018-01-30 13:34:18 -0500

    Add Batter Bucket and textures

---
+++ src/main/java/com/ununhexium116/betterbaking/items/ItemBatterBucket.java
@@ -0,0 +1,12 @@
+package com.ununhexium116.betterbaking.items;
+
+import net.minecraft.item.Item;
+
+public class ItemBatterBucket extends Item{
+
+    public ItemBatterBucket(String name) {
+        setUnlocalizedName(name);
+        setRegistryName(name);
+    }
+
+}
