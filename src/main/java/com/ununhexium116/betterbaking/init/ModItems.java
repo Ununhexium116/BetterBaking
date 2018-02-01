329ea581ac721de6871577ee879d6b1b3baa181c
Ununhexium116 <ununhaxium@gmail.com>
2018-02-01 13:11:16 -0500

    Add Cake Pan

--- src/main/java/com/ununhexium116/betterbaking/init/ModItems.java
+++ src/main/java/com/ununhexium116/betterbaking/init/ModItems.java
@@ -1,6 +1,7 @@
 package com.ununhexium116.betterbaking.init;
 
 import com.ununhexium116.betterbaking.BetterBaking;
+import com.ununhexium116.betterbaking.items.ItemCakePan;
 import com.ununhexium116.betterbaking.items.ItemDough;
 import com.ununhexium116.betterbaking.items.ItemFlour;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
@@ -17,24 +18,30 @@ public class ModItems {
 
     static Item flour;
     static Item dough;
+    static Item cakePan;
 
     public static void init() {
         flour = new ItemFlour("flour").setCreativeTab(CreativeTabs.FOOD).setMaxStackSize(64);
         dough = new ItemDough("dough").setCreativeTab(CreativeTabs.FOOD).setMaxStackSize(64);
+        cakePan = new ItemCakePan("cakePan").setCreativeTab(CreativeTabs.MISC).setMaxStackSize(16);
     }
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
-        event.getRegistry().registerAll(flour, dough);
+        event.getRegistry().registerAll(flour, dough, cakePan);
     }
 
     @SubscribeEvent
     public static void registerRenders(ModelRegistryEvent event) {
         registerRender(flour);
         registerRender(dough);
+        registerRender(cakePan);
     }
 
     private static void registerRender(Item item) {
         ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation( item.getRegistryName(), "inventory"));
+        if (item == cakePan) {
+            System.out.printf(item.getRegistryName().toString());
+        }
     }
 }
---
+++ src/main/java/com/ununhexium116/betterbaking/items/ItemCakePan.java
@@ -0,0 +1,12 @@
+package com.ununhexium116.betterbaking.items;
+
+import net.minecraft.item.Item;
+
+public class ItemCakePan extends Item {
+
+    public ItemCakePan(String name) {
+        setUnlocalizedName(name);
+        setRegistryName(name);
+    }
+
+}
